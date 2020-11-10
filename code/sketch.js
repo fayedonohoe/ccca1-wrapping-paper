c1c480d7c46026137704114bf9c86b5a49b9962a
Faye Donohoe <fayedonohoe@hotmail.com>
2020-11-10 22:16:38 +0000

    Added second parabola, rotating incorrectly

--- code/sketch.js
+++ code/sketch.js
@@ -1,59 +1,95 @@
 let numberOfLines = 10; //per quadrant
+let canW = 500;
+let canH = 500;
 
 function setup(){
   createCanvas(500,500);
-  colorMode(HSB, width, height); // change colour mode and defined the range as the width and height
+  colorMode(HSB, canW, canH); // change colour mode and defined the range as the canW and canH
   rectMode(CENTER);
 }
 
 function draw(){
 
-  let step = height/(numberOfLines*2);
-  let x = width/2;
+  let step = canH/(numberOfLines*2);
+  let x = canW/2;
   let y = 0;
 
-  numberOfLines = map(mouseX, 0,width, 3,15);
+  numberOfLines = map(mouseX, 0,canW, 3,15);
 
   background(0);
 
-  //translate(width/2, height/2);
+  //translate(canW/2, canH/2);
   stroke(180,97,97);
 
   //Centre Cross
-  line(width/2,0, width/2,height);
-  line(0,height/2, height,height/2);
+  line(canW/2,0, canW/2,canH);
+  line(0,canH/2, canH,canH/2);
 
-  // for each increment of y, x goes from centre to zero then centre to width
+  // for each increment of y, x goes from centre to zero then centre to canW
 
   //Quadrant 1
-  for (let i = 0; i <= height/2; i += step ){
-    line(width/2,y, x,height/2);
+  for (let i = 0; i <= canH/2; i += step ){
+    line(canW/2,y, x,canH/2);
     x -= step;
     y += step;
   }
 
   //Quadrant 3
-  for (let i = 0; i <= height/2; i += step ){
-    line(width/2,y, x,height/2);
+  for (let i = 0; i <= canH/2; i += step ){
+    line(canW/2,y, x,canH/2);
     x += step;
     y += step;
   }
 
-  //y = 0;
-
   //Quadrant 4
-  for (let i = 0; i <= height/2; i += step ){
-    line(width/2,y, x,height/2);
+  for (let i = 0; i <= canH/2; i += step ){
+    line(canW/2,y, x,canH/2);
     x += step;
     y -= step;
   }
 
-  //Quadrant 4
-  for (let i = 0; i <= height/2; i += step ){
-    line(width/2,y, x,height/2);
+  //Quadrant 2
+  for (let i = 0; i <= canH/2; i += step ){
+    line(canW/2,y, x,canH/2);
     x -= step;
     y -= step;
   }
+
+
+
+  // Smaller
+  //Quadrant 1
+
+  rotate(mouseY);
+  push();
+    for (let i = 0; i <= canH/2; i += step ){
+      line(canW/2,y, x,canH/2);
+      x -= step;
+      y += step;
+    }
+
+    //Quadrant 3
+    for (let i = 0; i <= canH/2; i += step ){
+      line(canW/2,y, x,canH/2);
+      x += step;
+      y += step;
+    }
+
+    //Quadrant 4
+    for (let i = 0; i <= canH/2; i += step ){
+      line(canW/2,y, x,canH/2);
+      x += step;
+      y -= step;
+    }
+
+    //Quadrant 2
+    for (let i = 0; i <= canH/2; i += step ){
+      line(canW/2,y, x,canH/2);
+      x -= step;
+      y -= step;
+    }
+  pop();
+
 
 }// end draw()
 
